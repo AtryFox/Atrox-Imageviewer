ad0de0a818e7e3a3d7a3a111ed7822ae029d2f4e
Benjamin Leuckefeld <code@deratrox.de>
2016-09-24 13:37:54 +0200

    implemented mustache

--- imgviewer/page/index.php
+++ imgviewer/page/index.php
@@ -15,6 +15,22 @@ $theme = "";
 
 
 include("config.php");
+
+// MUSTACHE ENGINE
+
+require 'assets/src/Mustache/Autoloader.php';
+Mustache_Autoloader::register();
+
+$m = new Mustache_Engine(array(
+	'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/views/', array('extension' => '.html')),
+	'partials_loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/views/', array('extension' => '.html')),
+));
+
+$data = new stdClass();
+$data->title = $title;
+$data->headerlink = $headerlink;
+$data->theme = $theme;
+
 $available = true;
 if (isset($_GET["image"])) $file = "../" . $_GET["image"];
 else $available = false;
@@ -31,6 +47,7 @@ foreach ($format as &$value) {
 
 if (!file_exists($file)) $available = false;
 
+$data->available = $available;
 
 $pageURL = 'http';
 if ($_SERVER["HTTPS"] == "on") {
@@ -43,24 +60,29 @@ if ($_SERVER["SERVER_PORT"] != "80" && $_SERVER["SERVER_PORT"] != "443") {
 	$pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
 }
 
-$current_img = explode("?", $pageURL)[0];
-$current_url = explode("?", $pageURL)[0] . "." . $file_type;
-
-?>
-
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<?php include("head.php"); ?>
-	</head>
-
-	<body>
-	<?php include("body.php"); ?>
-	</body>
-	</html>
-
-<?php
-if (file_exists("../.htaccess") && file_exists("welcome.php")) {
-	unlink("welcome.php");
+if ($_GET["image"] == "welcome" && file_exists("views/welcome.html")) {
+	$data->show_welcome = true;
+	$data->directory = dirname( __DIR__);
 }
-?>
+
+$data->pageURL = $pageURL;
+$data->current_img = explode("?", $pageURL)[0];
+$data->current_url = explode("?", $pageURL)[0] . "." . $file_type;
+$data->file = $file;
+$data->title_filename = str_replace("../", "", $file);
+$data->last_modified = date($dateformat, filemtime($file));
+
+echo $m->render("main", $data);
+
+if (file_exists("../.htaccess") && file_exists("views/welcome.html")) {
+	unlink("views/welcome.html");
+}
+
+
+
+
+
+
+
+
+
